3fc13f65d6750fd1cdbec8462854c7e82ee233da
Wafae <fouziwafae@gmail.com>
2017-12-05 21:33:58 +0100

    Commentaires

--- FileDialog.h
+++ FileDialog.h
@@ -2,6 +2,8 @@
 
 #include <string>
 
+/** Declaration of functions used in the FileDialog.cpp **/
+
 std::string openFileDialog(std::string extension);
 std::string openPathDialog();
 std::string saveFileDialog(std::string proposedFileNameFullPath);
