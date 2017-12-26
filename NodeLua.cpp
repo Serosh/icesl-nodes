22570e03cb0ba1017e3cd6663df8981b8557f964
mpierret <martin.pierret@depinfonancy.net>
2017-12-26 16:06:52 +0100

    Revert "Ajout de commentaires"
    This reverts commit 21d5976110b64672209f1181ed6e42b6adf90b4f.

--- NodeLua.cpp
+++ NodeLua.cpp
@@ -131,7 +131,6 @@ void Node::parse() {
 }
 
 
-
 //-------------------------------------------------------
 //write the master lua file by traversing the graph.
 //bool Node::writeMaster(ofstream& myfile)
